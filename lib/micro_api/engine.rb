2661b231586ab1c1a2c7290fd78c7a1a58a1adb0
lelered <lelered@gmail.com>
2023-03-18 17:15:01 +0100

    fix(rspec): intro test_framework

--- lib/micro_api/engine.rb
+++ lib/micro_api/engine.rb
@@ -2,5 +2,9 @@ module MicroApi
   class Engine < ::Rails::Engine
     isolate_namespace MicroApi
     config.generators.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
