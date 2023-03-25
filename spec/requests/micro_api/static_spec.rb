2403068e9768410609089e65a4a2f1d3c4be4b67
lelered <lelered@gmail.com>
2023-03-25 17:05:04 +0100

    test: added context

--- spec/requests/micro_api/static_spec.rb
+++ spec/requests/micro_api/static_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Statics", type: :request do
   describe "GET /" do
-    it "returns http success" do
-      get "#{MicroApi.routes_path}/"
-      expect(response).to have_http_status(:not_found)
-      expect(response.header['Content-Type']).to include 'application/json'
-      expect(json).not_to be_empty
+    context 'when the route does not exist' do
+      it "returns http success" do
+        get "#{MicroApi.routes_path}/"
+        expect(response).to have_http_status(:not_found)
+        expect(response.header['Content-Type']).to include 'application/json'
+        expect(json).not_to be_empty
+      end
     end
   end
 end
